8e05b92f12c33161803eeeb61a36e6c07f5cda92
Hunter Mellema <124718352+hpmellema@users.noreply.github.com>
2023-06-22 10:26:12 -0600

    Common shapes (#28)
    Add example for a package for sharing shapes between multiple models

---
+++ shared-model-examples/integ/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    val smithyGradleVersion: String by project
+
+    id("software.amazon.smithy").version(smithyGradleVersion)
+}
+
+
+// The test project doesn't produce a JAR.
+tasks["jar"].enabled = false
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+buildscript {
+    val smithyVersion: String by project
+
+    // Set the version of the CLI for the smithy gradle plugin to use when building this project
+    dependencies {
+        classpath("software.amazon.smithy:smithy-cli:$smithyVersion")
+    }
+}
+
+dependencies {
+    val smithyVersion: String by project
+
+    implementation(project(":shared-model-examples:common"))
+}
