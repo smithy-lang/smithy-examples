57d3187dbdb6d30524aa523af2c328087862a9ab
Hunter Mellema <124718352+hpmellema@users.noreply.github.com>
2023-10-31 13:36:25 -0600

    Bump gradle plugin version to 0.9.0 (#63)

--- custom-trait-examples/custom-annotation-trait/build.gradle.kts
+++ custom-trait-examples/custom-annotation-trait/build.gradle.kts
@@ -3,7 +3,7 @@ description = "A package used to define an annotation trait"
 plugins {
     `java-library`
     id("com.github.spotbugs").version("4.7.1")
-    id("software.amazon.smithy.gradle.smithy-jar").version("0.8.0")
+    id("software.amazon.smithy.gradle.smithy-jar").version("0.9.0")
 }
 
 java {
--- custom-trait-examples/integ/custom-annotation-trait-test/build.gradle.kts
+++ custom-trait-examples/integ/custom-annotation-trait-test/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("software.amazon.smithy.gradle.smithy-jar").version("0.8.0")
+    id("software.amazon.smithy.gradle.smithy-jar").version("0.9.0")
 }
 
 
