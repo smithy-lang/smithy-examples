8e40e20d1b67e5e5f17f07ddcd395b11fbe64b36
Hunter Mellema <hpm@amazon.com>
2025-02-11 15:51:48 -0700

    Add quickstart example for smithy-java

---
+++ smithy-java-examples/quickstart-java/server/build.gradle.kts
@@ -0,0 +1,39 @@
+description = "Cafe service server implementation"
+
+plugins {
+    `java-library`
+    application
+    // Executes smithy-build process to generate server stubs
+    id("software.amazon.smithy.gradle.smithy-base")
+}
+
+dependencies {
+    val smithyJavaVersion: String by project
+
+    // === Code generators ===
+    smithyBuild("software.amazon.smithy.java.codegen:plugins:$smithyJavaVersion")
+
+    // === Service model ===
+    implementation(project(":lib"))
+
+    // === Server dependencies ===
+    // Adds an HTTP server implementation based on netty
+    implementation("software.amazon.smithy.java:server-netty:$smithyJavaVersion")
+    // Adds the server implementation of the `RestJson1` protocol
+    implementation("software.amazon.smithy.java:aws-server-restjson:$smithyJavaVersion")
+}
+
+// Add generated source code to the compilation sourceSet
+afterEvaluate {
+    val serverPath = smithy.getPluginProjectionPath(smithy.sourceProjection.get(), "java-server-codegen")
+    sourceSets.main.get().java.srcDir(serverPath)
+}
+
+tasks.named("compileJava") {
+    dependsOn("smithyBuild")
+}
+
+// Use the application plugin to start the service via the `run` task.
+application {
+    mainClass = "io.smithy.java.server.example.CafeService"
+}
